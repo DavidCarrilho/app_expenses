434d7fb3aba3d113454c829ace16f037a69fcf99
DavidCarrilho <luan.david.oliver@gmail.com>
2022-02-22 00:48:23 -0300

    feat: create addTransaction method

--- lib/app/components/transaction_user.dart
+++ lib/app/components/transaction_user.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:expenses/app/components/trabsaction_list.dart';
 import 'package:expenses/app/components/transactions_form.dart';
 import 'package:expenses/app/models/models.dart';
@@ -10,22 +12,34 @@ class TransactionUser extends StatefulWidget {
   _TransactionUserState createState() => _TransactionUserState();
 }
 
-final List<Transaction> _transactions = [
-  Transaction(
-    id: 't1',
-    title: 'Nova Camisa',
-    value: 120.90,
-    date: DateTime.now(),
-  ),
-  Transaction(
-    id: 't2',
-    title: 'Cinema',
-    value: 60.90,
-    date: DateTime.now(),
-  )
-];
-
 class _TransactionUserState extends State<TransactionUser> {
+  final List<Transaction> _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Nova Camisa',
+      value: 120.90,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Cinema',
+      value: 60.90,
+      date: DateTime.now(),
+    )
+  ];
+
+  _addTransaction({String title, double value}) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+    setState(() {
+      _transactions.add((newTransaction));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
